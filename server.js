793e70404ed2a0d03bf5b6227db25ab6fb54cbcc
abel <abel_perez9@hotmail.com>
2022-09-22 11:11:44 -0500

    back to regular

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-     connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString : process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
